017aa91b8dd89f40e0c9feec3d4b046044f7ecc4
leonmhl <leonmoehl@outlook.de>
2023-12-08 23:43:28 +0200

    Week6

---
+++ Week6/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+
+string userName = "Darth Vader";
+
+Console.WriteLine(userName.Length);
+Console.WriteLine($"The last letter in your name is {userName[userName.Length-1]}.");
+
+//a-counter
+
+int counter = 0;
+
+for(int i = 0; i < userName.Length; i++)
+{
+    if (userName[i] == 'a')
+    {
+        counter++;
+    }
+}
+
+Console.WriteLine($"Your name has {counter} a letters");
+
+//replace 
+//a - 4
+
+string updatedUserName = userName.Replace('a', '4').Replace('e','3');
+
+Console.WriteLine(updatedUserName);
+
---
+++ Week6SecondPart/Program.cs
@@ -0,0 +1,27 @@
+﻿
+string[] fruitbasket = { "apples", "bananas", "oranges"};
+
+Console.WriteLine($"There are {fruitbasket.Length} fruitbasket in your basket.");
+
+for (int i = 0; i < fruitbasket.Length; i++)
+{
+    Console.WriteLine(fruitbasket[i]);
+}
+
+
+//Console.WriteLine(fruitbasket[0]);
+//Console.WriteLine(fruitbasket[1]);
+//Console.WriteLine(fruitbasket[2]);
+
+fruitbasket[0] = "pine-apple";
+fruitbasket[2] = "peaches";
+
+//Console.WriteLine(fruitbasket[0]);
+//Console.WriteLine(fruitbasket[1]);
+//Console.WriteLine(fruitbasket[2]);
+
+for (int i=0; i <fruitbasket.Length; i++)
+{
+    Console.WriteLine(fruitbasket[i]);
+}
+
